b42a9b1c4e2582d02c68a69977418d136a00cbbc
hmaccelerate <1057943176@qq.com>
2016-01-05 10:45:11 +0800

    完善包结构

---
+++ src/main/java/com/amazingfour/crms/dao/impl/Test.java
@@ -0,0 +1,7 @@
+package com.amazingfour.crms.dao.impl;
+
+/**
+ * Created by hmaccelerate on 2016/1/5.
+ */
+public class Test {
+}
---
+++ src/main/java/com/amazingfour/crms/service/impl/Test.java
@@ -0,0 +1,7 @@
+package com.amazingfour.crms.service.impl;
+
+/**
+ * Created by hmaccelerate on 2016/1/5.
+ */
+public class Test {
+}
